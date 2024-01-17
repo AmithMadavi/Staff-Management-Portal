50249534fe2f0da2ff933978e87d833eabb90337
SardarKhan103 <amithmadavi24@gmail.com>
2024-01-17 17:30:18 +0530

    first commit

---
+++ includes/mainfooter.php
@@ -0,0 +1,13 @@
+<footer class="bg-dark py-4 mt-auto" >
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="medium text-white">Project : Staff Recruitment portal 2023  &emsp;
+                    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                     MSc Computer Science 2021-23
+                    </div></div>
+                    <div class="col-auto">
+                    </div>
+                </div>
+            </div>
+        </footer>
